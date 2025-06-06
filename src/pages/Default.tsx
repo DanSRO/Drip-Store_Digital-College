e1a91d88a478914a7908d9b1f259ce29ecb03dff
daniel dantas <Daniel Dantas>
2025-06-06 10:12:15 -0300

    Corrige estrutura do projeto mantendo front e back

--- src/pages/Default.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "react-router-dom";
-export const Default = () =>{
-    return(
-        <>
-            <h1>Página não encontrada</h1>
-            <Link to="/">Voltar</Link>
-        </>
-    );
-}

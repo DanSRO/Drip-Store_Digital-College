142fb6a54cf2e6410671dff7295fed039c7bb6fb
daniel dantas <Daniel Dantas>
2025-06-06 11:33:20 -0300

    Created User componet to test and server backend

---
+++ backend/models/User.ts
@@ -0,0 +1,14 @@
+const DataTypes = require('sequelize');
+import { sequelize } from "../config/database";
+
+export const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
